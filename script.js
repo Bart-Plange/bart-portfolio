4d799db3a8c92a6293a46616ed54af4b9b289d92
Bart-Plange <87731837+Bart-Plange@users.noreply.github.com>
2023-06-21 21:19:41 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const hamburger = document.querySelector('.mobile-btn')
+const navlinks = document.querySelector('.mobile')
 
+hamburger.addEventListener('click',() =>{
+    hamburger.classList.toggle('open');
+    navlinks.classList.toggle('hidden')
+
+})
